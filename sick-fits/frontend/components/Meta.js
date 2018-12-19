bf82d76ebcd5fa99b12aab986d88b1d1898d920b
andreamayhirji <andreamayhirji@gmail.com>
2018-12-19 13:20:33 -0800

    sets up initial page components

---
+++ sick-fits/frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+//stateless functional component -- a lot of the state is handled in the other high order components
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" href="/static/nprogress.css" type="text/css" />
+    <title>Sick Fits!</title>
+  </Head>
+);
+
+export default Meta;
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,13 +1,15 @@
 import App, { Container } from 'next/app';
+import Page from '../components/Page';
 
+// the app component from next/app
 class MyApp extends App {
   render() {
     const { Component } = this.props;
-    
     return (
       <Container>
-        <p>Hey! I'm on every page</p>
+        <Page>
         <Component />
+        </Page>
       </Container>
     );
    }
