0a1085efce740befd1dd504e4a7d2cc64fe3b9bc
andreamayhirji <andreamayhirji@gmail.com>
2019-01-07 13:29:14 -0800

    adds queries and mutations to the schema

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info){
+    //db calls go here
+    // return [{ name: 'Basel' }, { name: 'Sybil' }];
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  },
+};
 
 module.exports = Query;
